7a8ca308e45072ea0eb751a3c2b26eb1988112a3
Yogesh More <65850665+moreyogesh19@users.noreply.github.com>
2020-06-17 17:06:57 +0530

    Java Practice

---
+++ Java_coding/src/FirstClass.java
@@ -0,0 +1,28 @@
+
+public class FirstClass {
+
+	
+	public void FirstMethod()
+	{
+	System.out.println("I am firstmethod of FirstClass");	
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" I am in First Class");
+		FirstClass firstclass = new FirstClass();
+		firstclass.FirstMethod();
+		SecondClass secondclass = new SecondClass();
+		secondclass.SecondMethod();
+		
+		String a = "JavaPractice";
+		System.out.println(a.concat(" on Eclipse"));
+		
+		System.out.println(a.indexOf("P"));
+	System.out.println(a.substring(4, 8));
+		
+		
+	}
+
+}
---
+++ Java_coding/src/SecondClass.java
@@ -0,0 +1,14 @@
+
+public class SecondClass {
+	
+	public void SecondMethod()
+	{
+		System.out.println("I am from secondMethod of SecondClass");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
